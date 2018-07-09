2c23cd701a97f0d2b9cff5b84e49a49ae9e869e5
Aeternus Atterratio <606837+Atterratio@users.noreply.github.com>
2018-07-09 10:42:31 +0300

    Fix no parent and depth atributes

--- flake8_prevent_fails/__init__.py
+++ flake8_prevent_fails/__init__.py
@@ -1,6 +1,6 @@
 import ast
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __all__ = ('FailsChecker', )
 
 
@@ -9,40 +9,52 @@ class PluginVisitor(ast.NodeVisitor):
         self.errors = []
 
     def visit_Index(self, node):
+        # TODO fix if indexing in try:else: statement
         index_error_handler = False
         index_error = (
             node.value.lineno,
             node.value.col_offset,
-            'PF101 Potential «IndexError: list index out of range» fail.',
+            'PF101 Potential IndexError fail.',
             type(self)
         )
-        depth = node.depth
+
         parent = node
-        for i in range(depth):
-            parent = parent.parent
-            if isinstance(parent, ast.Try):
-                for handler in parent.handlers:
-                    if handler.type:
-                        if handler.type.id == 'IndexError':
+        while True:
+            if getattr(parent, 'pf_parent', None):
+                parent = parent.pf_parent
+                if isinstance(parent, ast.Try):
+                    for handler in parent.handlers:
+                        if handler.type:
+                            if handler.type.id == 'IndexError':
+                                index_error_handler = True
+                                break
+                        else:
                             index_error_handler = True
-                            break
-                    else:
-                        index_error_handler = True
+                    break
+            else:
                 break
 
         if not index_error_handler:
             self.errors.append(index_error)
+
+        self.generic_visit(node)
+
+    def generic_visit(self, node):
+        super().generic_visit(node)
 
 
 class FailsChecker(object):
     name = 'flake8-prevent-fails'
     version = __version__
 
-    def __init__(self, tree):
+    def __init__(self, tree, filename, tokens):
+        for statement in ast.walk(tree):
+            for child in ast.iter_child_nodes(statement):
+                child.pf_parent = statement
+
         self.tree = tree
-        self.messages = [
-            (1, 0, 'PF101 Fancy header. Invalid.', type(self)),
-        ]
+        self.filename = filename
+        self.tokens = tokens
 
     def check_tree(self):
         visitor = PluginVisitor()
